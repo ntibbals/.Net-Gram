1ef05ec88a9fff0668ed66f4fa03f0a93801cbdb
ntibbals <ntibbals@gmail.com>
2019-02-13 11:36:19 -0800

    updated styling, layouts page

--- ASP.Gram/ASP.Gram/Data/GramDbContext.cs
+++ ASP.Gram/ASP.Gram/Data/GramDbContext.cs
@@ -21,14 +21,35 @@ namespace ASP.Gram.Data
                 {
                     ID = 1,
                     Author = "JTT",
-                    ImageURL = "image.jpg",
+                    ImageURL = "http://1.bp.blogspot.com/-rbBG3L514tQ/UD9eJuVrH1I/AAAAAAAAIo0/RKOGYNs0WwM/s400/Outer+Space+Wallpapers+6.jpg",
                     Details = "Check out the orcas!"
+                },
+                  new Post
+                {
+                    ID = 3,
+                    Author = "Captain Kirk",
+                    ImageURL = "http://1.bp.blogspot.com/-rbBG3L514tQ/UD9eJuVrH1I/AAAAAAAAIo0/RKOGYNs0WwM/s400/Outer+Space+Wallpapers+6.jpg",
+                    Details = "Captains log: We, have, found intelligent life on neptar."
+                },
+                  new Post
+                {
+                    ID = 4,
+                    Author = "Legolas",
+                    ImageURL = "http://1.bp.blogspot.com/-rbBG3L514tQ/UD9eJuVrH1I/AAAAAAAAIo0/RKOGYNs0WwM/s400/Outer+Space+Wallpapers+6.jpg",
+                    Details = "1 million orcs killed. Time to break out the champagne!"
+                },
+                  new Post
+                {
+                    ID = 5,
+                    Author = "John Snow",
+                    ImageURL = "http://1.bp.blogspot.com/-rbBG3L514tQ/UD9eJuVrH1I/AAAAAAAAIo0/RKOGYNs0WwM/s400/Outer+Space+Wallpapers+6.jpg",
+                    Details = "I may not know much but I know how to kill white walkes!"
                 },
                 new Post
                 {
                     ID = 2,
                     Author = "Miss Jackson",
-                    ImageURL = "image2.jpg",
+                    ImageURL = "http://1.bp.blogspot.com/-rbBG3L514tQ/UD9eJuVrH1I/AAAAAAAAIo0/RKOGYNs0WwM/s400/Outer+Space+Wallpapers+6.jpg",
                     Details = "I am for real!"
                 }
 
--- ASP.Gram/ASP.Gram/Migrations/20190213184048_seed3.Designer.cs
+++ ASP.Gram/ASP.Gram/Migrations/20190213184048_seed3.Designer.cs
@@ -3,14 +3,16 @@ using ASP.Gram.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASP.Gram.Migrations
 {
     [DbContext(typeof(GramDbContext))]
-    partial class GramDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190213184048_seed3")]
+    partial class seed3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
