8cc1a647fde2ee3e98fed3f956e8f72bf53e4d6a
ntibbals <ntibbals@gmail.com>
2019-02-12 19:23:18 -0800

    setup startup and db context

---
+++ ASP.Gram/ASP.Gram/Data/GramDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ASP.Gram.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Gram.Data
+{
+    public class GramDbContext : DbContext
+    {
+        public GramDbContext (DbContextOptions<GramDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Posts> Posts { get; set; }
+    }
+}
