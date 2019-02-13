17f1d0a9f50f678f2d7472d18dd601edae6f765f
ntibbals <ntibbals@gmail.com>
2019-02-12 21:10:59 -0800

    updated user sercrets and gitignore

--- ASP.Gram/ASP.Gram/Startup.cs
+++ ASP.Gram/ASP.Gram/Startup.cs
@@ -19,7 +19,9 @@ namespace ASP.Gram
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
