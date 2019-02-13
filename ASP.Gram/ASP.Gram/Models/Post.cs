73d95f5ad1d52d5830c76350e4fd538cb16ca731
ntibbals <ntibbals@gmail.com>
2019-02-13 11:49:19 -0800

    added model reqs

--- ASP.Gram/ASP.Gram/Models/Post.cs
+++ ASP.Gram/ASP.Gram/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace ASP.Gram.Models
     public class Post
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please provide a Username")]
+        [Display(Name = "Username")]
         public string Author { get; set; }
+        [Required(ErrorMessage = "Please provide an image")]
+        [Display(Name = "Image URL")]
         public string ImageURL { get; set; }
+        [Required(ErrorMessage = "Please provide a caption")]
+        [Display(Name = "Caption")]
         public string Details { get; set; }
         public string Comment { get; set; }
     }
