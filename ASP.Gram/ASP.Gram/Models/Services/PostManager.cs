4bdaef4e446ea729a292c9c9eeeacca189cef8fd
ntibbals <ntibbals@gmail.com>
2019-02-12 19:30:27 -0800

    setup base service

--- ASP.Gram/ASP.Gram/Models/Services/PostManager.cs
+++ ASP.Gram/ASP.Gram/Models/Services/PostManager.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using ASP.Gram.Data;
+using ASP.Gram.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASP.Gram.Models.Services
 {
-    public class PostManager
+    public class PostManager : IPosts
     {
+        private readonly GramDbContext _context;
+        public PostManager(GramDbContext context)
+        {
+            _context = context;
+        }
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Posts> FindPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Posts>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Posts post)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
